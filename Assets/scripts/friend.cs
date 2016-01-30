42eff629883e1e1787519397f34979918b5d0425
Aapo Ritvanen <aapo.ritvanen@gmail.com>
2016-01-30 02:20:18 +0200

    Player and friends following :cake:

---
+++ Assets/scripts/friend.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class friend : MonoBehaviour
+{
+	public GameObject followingObject;
+	public float speed;
+	public float radius;
+	public float nowDistance;
+	
+	void Update()
+	{
+		RaycastHit2D hit = Physics2D.Linecast (transform.position, followingObject.transform.position, 1 << followingObject.layer);
+
+		nowDistance = hit.distance;
+
+		if (hit.distance >= radius)
+			transform.position = Vector2.MoveTowards (transform.position, followingObject.transform.position, speed * Time.deltaTime);
+	}
+}
