e043f4c618d2fe3f189ad16ff66272028ddfbdb9
ArttuHuusko <arttu.huus@gmail.com>
2016-01-31 01:34:39 +0200

    alan hetki hetkeltä vihata aapoa

---
+++ Assets/scripts/skipCutscene.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class skipCutscene : MonoBehaviour {
+	
+	void Start () 
+	{
+	
+	}
+
+	void Update () 
+	{
+		if (Input.GetKey(KeyCode.Space))
+		{
+			Application.LoadLevel ("Project1");
+		}
+	}
+}
